2f80b0efa12df744eb54d480cf49b33b58e57758
yuzhijun <664281688@qq.com>
2015-12-16 09:46:47 +0800

    yuzhijun

---
+++ gen/com/lenovo/group/HelloMaven/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lenovo.group.HelloMaven;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ target/generated-sources/r/com/lenovo/group/HelloMaven/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.lenovo.group.HelloMaven;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+}
